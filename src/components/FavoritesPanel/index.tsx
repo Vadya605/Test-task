61419502540f8db113f92ab2f6dbe84ec51cf75f
Vadya <vadya.695@gmail.com>
2023-10-09 20:10:48 +0300

    Changing the rendering of favorite cards

--- src/components/FavoritesPanel/index.tsx
+++ src/components/FavoritesPanel/index.tsx
@@ -1,39 +1,47 @@
 import { Box } from '@mui/material'
 
-import BackIcon from '../../assets/img/Arrow.svg'
-import { useAppDispatch, useTypeSelector } from "../../hooks/redux";
-import { SelectedFavoriteServices } from "../../store/reducers";
+import BackIcon from '@/assets/img/Arrow.svg'
+import { useAppDispatch, useTypeSelector } from "@/hooks/redux";
+import { SelectedFavoriteServices } from "@/store/reducers";
 import CollapsedCard from "../CardFavorite/CollapsedCard";
 import ExpandedCard from "../CardFavorite/ExpandedCard";
-import { HeaderPanel,IconBack } from "./styled";
+import { HeaderPanel, IconBack } from "./styled";
 
 
-export default function FavoritesPanel(){
+export default function FavoritesPanel() {
     const dispatch = useAppDispatch()
-    const {place_id: selectedFavoriteId} = useTypeSelector(state => state.SelectedFavorite)
-    const {favorites} = useTypeSelector(state => state.Favorites)
-    
-    return(
+    const { place_id: selectedFavoriteId } = useTypeSelector(state => state.SelectedFavorite)
+    const { favorites } = useTypeSelector(state => state.Favorites)
+    const selectedFavorite = favorites.find(item => item.place_id === selectedFavoriteId)
+
+    const handleClickBack = () => {
+        dispatch(SelectedFavoriteServices.actions.setSelected(''))
+    }
+
+    const renderCollapsedCards = () => {
+        return favorites.map(favoriteItem => (
+            <CollapsedCard
+                favoriteItem={favoriteItem}
+                key={favoriteItem.place_id}
+            />
+        ))
+    }
+
+    return (
         <Box>
             <HeaderPanel>
-                { selectedFavoriteId? 
-                    <IconBack src={BackIcon} onClick={() => dispatch(SelectedFavoriteServices.actions.setSelected(null))} />
-                    :null
-                }
+                {selectedFavorite ? <IconBack src={BackIcon} alt='Back' onClick={handleClickBack} /> : null}
                 <h3>Избранное</h3>
             </HeaderPanel>
             {
-                selectedFavoriteId? 
+                selectedFavorite ? (
                     <ExpandedCard
-                        favoriteItem={ favorites.find(item => item.place_id === selectedFavoriteId) }
-                        key={selectedFavoriteId}
+                        favoriteItem={selectedFavorite}
+                        key={selectedFavorite.place_id}
                     />
-                    :favorites.map(favoriteItem => 
-                        <CollapsedCard
-                            favoriteItem={ favoriteItem }
-                            key={favoriteItem.place_id}
-                        />
-                    )
+                ) : (
+                    renderCollapsedCards()
+                )
             }
         </Box>
     )
