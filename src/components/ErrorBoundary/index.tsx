5c29cad8adab409e0922367c1846c025f55aa75c
Vadya <vadya.695@gmail.com>
2023-10-23 10:45:41 +0300

    feat: adding the getDerivedStateFromError method to the error boundary

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -1,13 +1,6 @@
-import { Component, ErrorInfo, ReactNode, cloneElement } from 'react';
+import { Component, ErrorInfo } from 'react';
+import { ErrorBoundaryProps, ErrorBoundaryState } from './interfaces';
 
-interface ErrorBoundaryProps {
-    children: ReactNode;
-    fallback: ReactNode;
-}
-
-interface ErrorBoundaryState {
-    hasError: boolean;
-}
 
 class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
     constructor(props: ErrorBoundaryProps) {
@@ -22,19 +15,15 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
         this.setState({ hasError: true });
     }
 
-    handleRetry = () => {
-        console.log('Retry');
-        
-        this.setState({
-            hasError: false,
-        });
+    static getDerivedStateFromError(error: Error) {
+        return { hasError: true }
     }
 
     render() {
         if (this.state.hasError) {
-            // return this.props.fallback
-            return cloneElement(this.props.fallback as React.ReactElement, this.handleRetry);
+            return this.props.fallback
         }
+        
         return this.props.children;
     }
 }
