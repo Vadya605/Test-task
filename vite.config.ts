fb388a8992f1f12944cf8662fd4ab7058b47b946
Vadya <vadya.695@gmail.com>
2023-11-01 15:45:01 +0300

    feat: vite configuration for build

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,23 @@ import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    define: {
-      'process.env': env
-    },
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': '/src',
-        "public": "/public",
-      },
-    },
-  }
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        define: {
+            'process.env': env
+        },
+        plugins: [react()],
+        resolve: {
+            alias: {
+                '@': '/src',
+                "public": "/public",
+            },
+        },
+        build: {
+            minify: 'terser',
+            chunkSizeWarningLimit: 2000,
+            publicDir: '/public',
+            sourcemap: true
+        }
+    }
 })
