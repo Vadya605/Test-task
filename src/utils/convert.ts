c406c5b7373464d300feb85abc1c10a72d2a8228
Vadya <vadya.695@gmail.com>
2023-11-09 11:56:54 +0300

    fix: adding a description for places

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -4,7 +4,7 @@ export function convertPlaceResultToFavorite(placeResult: google.maps.places.Pla
     const favorite: IFavorite = {
         place_id: placeResult.place_id || '',
         name: placeResult.name || '',
-        description: 'description', // как получить описание места
+        description: `${placeResult.name} ${placeResult.vicinity}`,
         photo:  placeResult.photos?.[0]?.getUrl() ||  '/public/doesntExist.png',
         icon: placeResult.icon || '',
         location: {
